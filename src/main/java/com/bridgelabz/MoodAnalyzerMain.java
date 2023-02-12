6471b186af8dec8ee621b020564e6bae9d1dc65e
unknown <gownideepu23@gmail.com>
2023-02-12 12:54:40 +0530

    added

---
+++ src/main/java/com/bridgelabz/MoodAnalyzerMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class MoodAnalyzerMain {
+    public MoodAnalyzerMain(){
+    }
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
